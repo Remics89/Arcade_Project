b4f751fd5458138432864c42c93f44623e9c121f
Matthew <Remics89@gmail.com>
2022-09-14 12:12:21 +0000

    feat: added functions to build starting board

--- app.js
+++ app.js
@@ -1,37 +1,61 @@
-let gameStart = {
-    playerCount: modeSelection(),
-    playerName: players(),
+const gameStart = {
+    playerName: playerNames(),
     gameBoard: buildBoard(),
 };
 
-function modeSelection() {
+function playerNames() {
     let div = document.createElement("div");
-    let playerInput = document.createElement("input");
-    let body = document.querySelector("body");
-    div.style = `
-        opacity: 1;
-        transition-duration: 20s;
-        transition-property: opacity;
-        `;
+    div.id = "player-names";
+
+    let nameInput1 = document.createElement("input");
+    let nameInput2 = document.createElement("input");
+    nameInput1.id = "inputbox1";
+    nameInput2.id = "inputbox2";
+
+    let inputSection = document.getElementById("progress");
+    let submitNames = document.createElement("button");
+    submitNames.textContent = "Start Game";
+
+    nameInput2.disabled = true;
+    nameInput2.placeholder = "Computer";
+
+    submitNames.id = "submitNames";
+
+    div.appendChild(nameInput1);
+    div.appendChild(nameInput2);
+
+    div.appendChild(submitNames);
+    inputSection.appendChild(div);
 }
 
-function players() {}
+document.getElementById("input1").addEventListener("click", () => {
+    document.getElementById("inputbox2").disabled = true;
+    document.getElementById("inputbox2").placeholder = "Computer";
+});
+
+document.getElementById("input2").addEventListener("click", () => {
+    document.getElementById("inputbox2").disabled = false;
+    document.getElementById("inputbox2").value = "";
+    document.getElementById("inputbox2").placeholder = "";
+});
 
 function buildBoard() {
     let board = [null, null, null, null, null, null, null, null, null];
-    let table = document.createElement("table");
-    document.body.appendChild(table)[-1];
+    let table = document.querySelector("table");
     table.className = "gameBoard";
-    table.id = 'fader';
+    table.id = "fader";
     for (let i = 0; i < board.length; i++) {
         let div = document.createElement("div");
         div.className = "cell";
+        div.style = `width: 120px;
+        height: 120px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        cursor: pointer;
+        font-size: xx-large;
+        border: 2px solid black;`;
         table.appendChild(div);
-        div.innerText = "X";
     }
-
-    table.style = `opacity: 1;
-    transition-duration: 10s;
-    transition-property: opacity;`
-
 }
